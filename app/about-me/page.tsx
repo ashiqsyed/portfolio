99b5c275492e4cd65902b1888a4ec55cf9606a6d
ashiqsyed <ahs57816@uga.edu>
2025-06-07 01:05:18 -0400

    completed about me page and fixed css

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -6,10 +6,10 @@ import Image from 'next/image'
 export default function AboutMe() {
     
     return (
-        <div className="bg-gray-900 min-h-screen text-gray-100 flex items-center justify-center flex-col">
-            <div className=" w-2/3">
-                <div className="w-full h-1/3 flex m-8">
-                    
+        <div className="bg-gray-900 text-gray-100 flex items-center justify-center ">
+            <div className="w-3/4 flex flex-col">
+            {/* first third */}
+                <div className="w-full flex m-8 p-8">
                     <div className="w-1/3 h-full flex items-center justify-center">
                         <Image 
                             src="/ashiq.jpg"
@@ -19,7 +19,7 @@ export default function AboutMe() {
                             // className="rounded-full"
                         />
                     </div>
-                    <div className="w-1/3 h-full flex flex-col items-center justify-center">
+                    <div className="w-1/3 flex flex-col items-center justify-center">
                         <h1 className="text-3xl w-full">About Me</h1>
                         <p className="w-full">I'm a recent Computer Science graduate from the University of Georgia with a background in software engineering, having strengthened my skills through coursework and by participating in hackathons.</p>                                     
                         <p className="w-full">While I structured my coursework around software engineering, I also developed an interest in other fields of computer science - such as data, cybersecurity, and IT - through other courses and am open to roles across the field, where I can continue to learn, grow, and contribute.</p>
@@ -32,16 +32,17 @@ export default function AboutMe() {
                             alt="UGAHacks 2025"
                         />
                     </div>
-                    
                 </div>
 
-                <div className="w-full h-1/3 flex m-8 border border-white">
-                    <div className="w-2/3 h-full flex justify-center items-center border border-white">
+                {/* second third */}
+                <div className="w-full flex m-8 p-8">
+                    <div className="w-2/3 flex justify-center items-center ">
                         <div>
-                            <p className="w-full border border-orange-500">I now stay active by working on personal projects, learning about new technologies and expanding my skills.</p>
+                            <p className="w-full ">I now stay active by working on personal projects, learning about new technologies and expanding my skills.</p>
                             <p className="w-full">I am currently seeking a full-time entry-level position or internship where I am able to make an impact.</p>
                             <p className="w-full"> Aside from programming, I enjoy playing guitar, going to the gym, traveling, and playing video games. I have gotten more into bouldering and cooking. I also enjoy discovering new types of music and expanding my taste.</p>
                         </div>
+                        {/* include the last few pictures here maybe? */}
                         
                     </div>
                     <div className="w-1/3 h-full  flex items-center justify-center">
@@ -55,14 +56,6 @@ export default function AboutMe() {
                                     alt="UGA Hacks 2025"
                                 />
                             </CarouselItem>
-                            {/* <CarouselItem>
-                                <Image 
-                                    src="/ny2.jpg"
-                                    width={400}
-                                    height={400}
-                                    alt="UGA Hacks 2025"
-                                />
-                            </CarouselItem> */}
                             <CarouselItem>
                                 <Image 
                                     src="/ny3.jpg"
@@ -119,14 +112,6 @@ export default function AboutMe() {
                                     alt="UGA Hacks 2025"
                                 />
                             </CarouselItem>
-                            {/* <CarouselItem>
-                                <Image 
-                                    src="/ha1.jpg"
-                                    width={400}
-                                    height={400}
-                                    alt="UGA Hacks 2025"
-                                />
-                            </CarouselItem> */}
                         </CarouselContent>
                         <CarouselPrevious />
                         <CarouselNext />
@@ -134,8 +119,9 @@ export default function AboutMe() {
                     </div>
                 </div>
 
-                <div className="w-full h-1/3 flex items-center justify-center m-8">
-                    <iframe className="w-1/2" src="https://open.spotify.com/embed/playlist/1tgoewEoIaoLZHfVxi3Upz?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                {/* third third */}
+                <div className="w-full flex items-center justify-center m-8 p-8">
+                    <iframe className="w-1/2" src="https://open.spotify.com/embed/playlist/1tgoewEoIaoLZHfVxi3Upz?utm_source=generator" width="100%" height="352"  allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </div>
             </div>
             
@@ -143,13 +129,3 @@ export default function AboutMe() {
         </div>
     )
 }
-
-{/* <div className="w-1/2 h-7/8 flex items-center justify-center">
-                <div className="w-full h-2/3 overflow-auto">
-                    
-                    
-                </div>
-                <div className="border border-white w-full h-2/3">
-                    Carousel Here
-                </div>
-            </div> */}
