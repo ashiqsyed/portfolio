2da0e3865413fddb195df34c4408da96f70e8e23
ashiqsyed <ahs57816@uga.edu>
2025-06-10 00:23:37 -0400

    fixed responsiveness on footer.tsx and the main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,15 @@ import './globals.css'
 
 export default function Home() {
   return (
-    <div className="bg-gray-900 h-screen w-full flex items-center justify-center">
+    <div className="bg-gray-900 w-full min-h-screen flex items-center justify-center">
       <div className="text-gray-100 w-1/2 h-1/2 flex flex-col items-center">
           <div className="text-2xl mb-4">Hello, I'm</div>          
-          <div className="text-6xl mt-4 mb-4">
+          <div className="text-6xl mt-4 mb-4 h-48 flex items-center justify-center text-center sm:h-32">
             <TypeAnimation 
               sequence={[
                 'Ashiq Syed',
                 1000, 
-                '',
-                500
+
               ]}
               wrapper="span"
               speed={50}
@@ -24,9 +23,9 @@ export default function Home() {
             />
           </div>
           <div className="text-3xl text-center mt-4 mb-4">Recent computer science graduate from the University of Georgia</div>
-          <div className=" w-3/4 flex items-center justify-around text-3xl mt-4">
-            <Link className="relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[1px] after:bg-gray-100 after:w-0 hover:after:w-full after:transition-all after:duration-150  " href="/projects">Check out my work</Link>
-            <Link className="relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[1px] after:bg-gray-100 after:w-0 hover:after:w-full after:transition-all after:duration-150 " href="contact-me">Contact me</Link>
+          <div className=" w-3/4 flex items-center justify-around text-3xl mt-4 sm:flex-row flex-col text-center">
+            <Link className=" m-4 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[1px] after:bg-gray-100 after:w-0 hover:after:w-full after:transition-all after:duration-150  " href="/projects">Check out my work</Link>
+            <Link className="m-4 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[1px] after:bg-gray-100 after:w-0 hover:after:w-full after:transition-all after:duration-150 " href="contact-me">Contact me</Link>
           </div>          
       </div>
     </div>
